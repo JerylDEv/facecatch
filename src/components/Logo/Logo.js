42eb046e40fcebef59641bbb2cd72a10c4abe0f8
Jeryl Donato Estopace <jeryldev@gmail.com>
2020-06-01 23:33:47 +0800

    minor update on the logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,14 +5,14 @@ import brain from './brain.png';
 
 const Logo = () => {
   return (
-    <div className='ma4 mt0'>
+    <div className='ma4 mt0' id='logo-item'>
       <Tilt
         className='Tilt br2 shadow-2'
         options={{ max: 55 }}
-        style={{ height: 150, width: 150 }}
+        // style={{ height: 150, width: 150 }}
       >
-        <div className='Tilt-inner'>
-          <img src={brain} alt='logo'></img>
+        <div className='Tilt-inner pa3'>
+          <img style={{ paddingTop: '5px' }} src={brain} alt='logo'></img>
         </div>
       </Tilt>
     </div>
