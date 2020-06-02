b924afdc7445a128affc1c82f82a29b34c6d5ff6
JerylDEv <jeryldev@gmail.com>
2020-06-02 14:45:53 +0800

    create onInputChange and onButtonSubmit functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -9,20 +9,34 @@ import Particles from 'react-particles-js';
 const particlesOptions = {
   particles: {
     number: {
-      value: 30,
+      value: 100,
       density: { enable: true, value_area: 800 },
     },
   },
 };
 
 function App() {
+  const [input, setInput] = useState('');
+
+  const onInputChange = (event) => {
+    // console.log(event.target.value);
+    setInput(event.target.value);
+  };
+
+  const onButtonSubmit = () => {
+    console.log(input);
+  };
+
   return (
     <div className='App'>
       <Particles className='particles' params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm
+        onInputChange={onInputChange}
+        onButtonSubmit={onButtonSubmit}
+      />
       {/* <FaceRecognition /> */}
     </div>
   );
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
@@ -9,10 +9,17 @@ const ImageLinkForm = () => {
       </p>
       <div className='center-align-items'>
         <div className='form center-align-items pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='text' name='' id='' />
+          <input
+            className='f4 pa2 w-70 center'
+            type='text'
+            name='input-url'
+            id='input-url'
+            onChange={onInputChange}
+          />
           <button
             className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
             type='submit'
+            onClick={onButtonSubmit}
           >
             Detect
           </button>
