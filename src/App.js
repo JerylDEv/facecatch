a1c73a125945b902a479dffed360b8f537d4e599
JerylDEv <jeryldev@gmail.com>
2020-06-02 13:38:34 +0800

    create Rank Component and install react-particles-js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Navigation from './components/Navigation/Navigation';
+import Rank from './components/Rank/Rank';
 
 function App() {
   return (
     <div className='App'>
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
