fafd69acbac98db54fe96aeb4a6b0bc426abee4a
JerylDEv <jeryldev@gmail.com>
2020-06-08 19:18:23 +0800

    added ability to use signin API

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Signin = ({ onRouteChange }) => {
+  const [signInEmail, setSignInEmail] = useState('');
+  const [signInPassword, setSignInPassword] = useState('');
+
+  const onEmailChange = (event) => {
+    setSignInEmail(event.target.value);
+  };
+
+  const onPasswordChange = (event) => {
+    setSignInPassword(event.target.value);
+  };
+
+  const onSignInSubmit = () => {
+    fetch('http://localhost:4000/signin', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: signInEmail,
+        password: signInPassword,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data === 'success') {
+          onRouteChange('home');
+        } else {
+          alert('username or password does not match.');
+        }
+      });
+  };
+
   return (
     <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
       <main className='pa4 black-80'>
@@ -16,6 +46,7 @@ const Signin = ({ onRouteChange }) => {
                 type='email'
                 name='email-address'
                 id='email-address'
+                onChange={onEmailChange}
               />
             </div>
             <div className='mv3'>
@@ -27,12 +58,13 @@ const Signin = ({ onRouteChange }) => {
                 type='password'
                 name='password'
                 id='password'
+                onChange={onPasswordChange}
               />
             </div>
           </fieldset>
           <div className=''>
             <input
-              onClick={() => onRouteChange('home')}
+              onClick={() => onSignInSubmit()}
               className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib'
               type='submit'
               value='Sign in'
@@ -40,7 +72,9 @@ const Signin = ({ onRouteChange }) => {
           </div>
           <div className='lh-copy mt3'>
             <p
-              onClick={() => onRouteChange('register')}
+              onClick={() => {
+                return onRouteChange('register');
+              }}
               className='f6 link dim black db pointer'
             >
               Register
